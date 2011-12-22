ab98f121602b068f3f87ca4f091c307266d2ae93
Leon Zhang <leoncamel@gmail.com>
2011-12-23 04:48:21 +0900

    add site-start.d/init_fci.el

---
+++ site-start.d/init_fci.el
@@ -0,0 +1,30 @@
+;;; init_fci.el --- fill-column-indicator mode settings
+
+;; Copyright (C) 2011  Le0n Zh4ng
+
+;; Author: Le0n Zh4ng <leoncamel@gmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+(provide 'init_fci)
+;;; init_fci.el ends here

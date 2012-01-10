157f2635b33f71f2426e84ebb3a6e7c0de7cdf24
Leon Zhang <leoncamel@gmail.com>
2012-01-11 05:02:43 +0900

    add customization settings for ido-mode

--- site-start.d/init_ido.el
+++ site-start.d/init_ido.el
@@ -28,5 +28,16 @@
 (setq ido-save-directory-list-file
       (my-file-path-join dotfiles-dir "var/ido.last"))
 
+(ido-mode t)
+(ido-ubiquitous t)
+(setq ido-enable-prefix nil
+      ido-enable-flex-matching t
+      ido-auto-merge-work-directories-length nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess
+      ido-use-virtual-buffers t
+      ido-handle-duplicate-virtual-buffers 2
+      ido-max-prospects 10)
+
 (provide 'init_ido)
 ;;; init_ido.el ends here

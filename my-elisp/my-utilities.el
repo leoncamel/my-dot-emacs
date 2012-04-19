192f3914c14342c02d0ed57a806f9417ae7d8057
Leon leon-new-desktop <leoncamel@gmail.com>
2012-04-19 16:45:55 +0900

    add some utility function to delete adjacent whitespace
    Hmm, don't remember when did I add these fucntions. :(

--- my-elisp/my-utilities.el
+++ my-elisp/my-utilities.el
@@ -35,5 +35,37 @@
   (shell-command
    (concat (expand-file-name leon-sublime-executable-path) " " (buffer-name))))
 
+;;
+;; Delete adjacent whitespaces
+;;
+
+;; reference : http://www.emacswiki.org/emacs/DeletingWhitespace
+;; To delete whitespace from point to next word:
+(defun whack-whitespace (arg)
+  "Delete all white space from point to the next word.  With prefix ARG
+    delete across newlines as well.  The only danger in this is that you
+    don't have to actually be at the end of a word to make it work.  It
+    skips over to the next whitespace and then whacks it all to the next
+    word."
+  (interactive "P")
+  (let ((regexp (if arg "[ \t\n]+" "[ \t]+")))
+    (re-search-forward regexp nil t)
+    (replace-match "" nil nil)))
+
+;; To delete adjacent whitespace forward:
+(defun delete-horizontal-space-forward () ; adapted from `delete-horizontal-space'
+  "*Delete all spaces and tabs after point."
+  (interactive "*")
+  (delete-region (point) (progn (skip-chars-forward " \t") (point))))
+
+;; Backward:
+(defun backward-delete-char-hungry (arg &optional killp)
+  "*Delete characters backward in \"hungry\" mode.
+    See the documentation of `backward-delete-char-untabify' and
+    `backward-delete-char-untabify-method' for details."
+  (interactive "*p\nP")
+  (let ((backward-delete-char-untabify-method 'hungry))
+    (backward-delete-char-untabify arg killp)))
+
 (provide 'my-utilities)
 ;;; my-utilities.el ends here
